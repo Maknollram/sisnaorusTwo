da06ed3edecd9925c6fa13c9e1ea17b8dbd697bf
Maknollram <maknollram@hotmail.com>
2023-03-10 23:39:26 -0300

    add toast

--- src/main.js
+++ src/main.js
@@ -4,7 +4,37 @@ import router from "./router";
 import { createPinia } from "pinia";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import Toast from "vue-toastification";
+// Import the CSS or use your own!
+import "vue-toastification/dist/index.css";
+import axios from "./http-common";
+
+const toastOptions = {
+  // You can set your default options here
+  position: "top-right",
+  timeout: 3000,
+  hideProgressBar: false,
+  icon: true,
+  closeButton: false,
+  pauseOnHover: true,
+  closeOnClick: true,
+};
 
 loadFonts();
 
-createApp(App).use(router).use(createPinia()).use(vuetify).mount("#app");
+axios.interceptors.request.use((config) => {
+  // console.log(config);
+  return config;
+});
+
+axios.interceptors.response.use((response) => {
+  // console.log(response);
+  return response;
+});
+
+createApp(App)
+  .use(router)
+  .use(createPinia())
+  .use(vuetify)
+  .use(Toast, toastOptions)
+  .mount("#app");
--- src/store/pokemons.js
+++ src/store/pokemons.js
@@ -1,49 +1,58 @@
 import DataService from "../services/DataService";
 import { defineStore } from "pinia";
+import { useToast } from "vue-toastification";
+import consts from "../assets/json/consts.json";
 
-export const usePokemonStore = defineStore('pokemon',{
+const toast = useToast();
+
+const toastMessages = consts.toastMessages;
+
+export const usePokemonStore = defineStore("pokemon", {
   state: () => {
-    return { 
+    return {
       names: {},
       pokemon: {},
       imgUrl:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/",
-      dialogPokemon: false
-    }
+      dialogPokemon: false,
+    };
   },
-  getters: {
-    
-  },
+  getters: {},
   actions: {
-    setPokemon(pokemon){
-      this.pokemon = pokemon
+    setPokemon(pokemon) {
+      this.pokemon = pokemon;
     },
-    setDialogPokemon(bool){
-      this.dialogPokemon = bool
+    setDialogPokemon(bool) {
+      this.dialogPokemon = bool;
     },
     async listNames() {
       try {
         const res = await DataService.getAll();
-        this.names  = res.data.results.slice(0, 1008);
+        this.names = res.data.results.slice(0, 1008);
+        toast.info(toastMessages.catch);
+        toast.success(toastMessages.trainer);
       } catch (e) {
         console.log(e);
+        toast.error(toastMessages.list);
       }
     },
     async getOneByName({ name }) {
       try {
         const res = await DataService.getOneByName(name);
-        this.pokemon = res.data
+        this.pokemon = res.data;
       } catch (e) {
         console.log(e);
+        toast.error(toastMessages.pokemon);
       }
     },
-    async getOneById ({ id }) {
+    async getOneById({ id }) {
       try {
         const res = await DataService.getOneById(id);
-        this.pokemon = res.data
+        this.pokemon = res.data;
       } catch (e) {
         console.log(e);
+        toast.error(toastMessages.pokemon);
       }
     },
   },
-})
+});
